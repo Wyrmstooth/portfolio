e321d6ade06fea9395de135fb4b4540ee94905ca
umer <wyrmstooth0@gmail.com>
2024-03-17 23:34:24 +0500

    responsivness

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,23 +5,23 @@ import logo from "@/../public/vercel.svg";
 
 const Navbar = () => {
   return (
-    <>
-      <div className="flex mt-12 ml-72 mb-9 font-semibold ">
+    <div className="flex flex-col items-center w-full">
+      <div className="flex items-center justify-between w-full max-w-7xl mt-12 mb-9 font-semibold">
         <span className="text-4xl font-bold">
-          Umer<span className=" text-orange-600 mr-56 ">.</span>
+          Umer<span className="text-orange-600 mr-4">.</span>
         </span>
-        <nav className=" flex gap-10">
+        <nav className="flex gap-10">
           <NavItem>Home</NavItem>
           <NavItem>Project</NavItem>
           <NavItem>About</NavItem>
           <NavItem>Services</NavItem>
         </nav>
-        <button className=" border border-black absolute p-2  px-6 hover:bg-black hover:text-white ml-[825px] -mt-3 transition duration-300">
+        <button className="border border-black p-2 px-6 hover:bg-black hover:text-white transition duration-300">
           Let&apos;s Chat
         </button>
       </div>
-      <hr className="mx-72"></hr>
-    </>
+      <hr className="w-full max-w-7xl"></hr>
+    </div>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,39 +3,51 @@ import richard from "@/../public/richard.png";
 
 export default function Home() {
   return (
-    <main className="ml-60 inline-flex flex-grow-0 gap-5 items-start">
-      <div className=" mt-28">
-        <div className="text-7xl font-extrabold shr flex flex-col w-92">
+    <main className=" ml-16 mr-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex flex-col lg:flex-row items-center justify-between">
+      <div className="max-w-xl mt-4 lg:mt-0">
+        <div className="text-5xl sm:text-4xl lg:text-7xl font-extrabold mb-4 lg:mb-8 flex flex-col">
           <span> Hi! I am</span>
-          <span className=" text-2xl bg-blue-600 text-white p-5 rounded-full absolute  ml-[300px]">
+          <span className=" text-2xl bg-blue-600 text-white p-4 rounded-full mt-2 absolute ml-[300px] ">
             Web Developer
           </span>
           <span>Umer Jahangir</span>
         </div>
-        <p className=" text-xl mt-14 ">
-          I have been developing sites for aver <strong>an year</strong> as a
+        <p className="text-base sm:text-lg lg:text-xl mb-4 lg:mb-8">
+          I have been developing sites for over <strong> a year</strong> as a
           web developer
         </p>
-        <button className=" bg-orange-600 text-white w-32 h-full p-5 font-bold text-lg mt-10 hover:bg-orange-700 transition-all">
+        <button className="bg-orange-600 text-white w-24 sm:w-32 h-full p-3 sm:p-4 px-4 sm:px-5 font-bold text-base sm:text-lg mb-4 lg:mb-8 hover:bg-orange-700 transition-all">
           Hire me
         </button>
-        <span className=" ml-12 font-semibold">Projects -&gt;</span>
-        <div className=" mt-12">
-          <p className=" text-2xl font-bold">+84 </p>
-          <p className=" w-36 text-lg">Clients on work worldwide</p>
+        <span className="ml-2 sm:ml-12 font-semibold">Projects -&gt;</span>
+        <div className="mt-6 lg:mt-12">
+          <p className="text-lg sm:text-2xl lg:text-3xl font-bold">+84 </p>
+          <p className="w-24 sm:w-36 lg:w-48 text-base sm:text-lg lg:text-xl mb-4 lg:mb-8">
+            Clients on work worldwide
+          </p>
         </div>
-        <div className=" mt-12 flex gap-12 mb-32">
+        <div className="flex flex-col lg:flex-row mb-16 lg:mb-32">
           <div>
-            <p className=" text-2xl font-bold">572+</p>
-            <p className=" w-36 text-lg">Projects done</p>
+            <p className="text-lg sm:text-2xl lg:text-3xl font-bold">572+</p>
+            <p className="w-24 sm:w-36 lg:w-48 text-base sm:text-lg lg:text-xl mb-4 lg:mb-8">
+              Projects done
+            </p>
           </div>
           <div>
-            <p className=" text-2xl font-bold">Contact</p>
-            <p className=" w-36 text-lg">wyrmstooth0@gmail.com</p>
+            <p className="text-lg sm:text-2xl lg:text-3xl font-bold">Contact</p>
+            <p className="w-24 sm:w-36 lg:w-48 text-base sm:text-lg lg:text-xl mb-4 lg:mb-8">
+              wyrmstooth0@gmail.com
+            </p>
           </div>
         </div>
       </div>
-      <Image src={richard} width={500} height={500} alt="richard" />
+      <Image
+        src={richard}
+        width={500}
+        height={500}
+        alt="richard"
+        className="w-full lg:w-1/2"
+      />
     </main>
   );
 }
